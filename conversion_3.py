e3aa47dd47b65a64677202f6c335ee603d81db47
LeonardSchwenk <leonardschwenk@gmail.com>
2018-05-06 11:52:34 +0300

    trained with 150 epoches

--- conversion_3.py
+++ conversion_3.py
@@ -1,10 +1,10 @@
 import coremltools
 
-output_labels = ['0', '1', '2', '3', '4', '5', '6', '7']
+output_labels = ['Banana', 'Battery', 'Computer', 'Glass bottle', 'Light bulb', 'Paper', 'Phone', 'Plastic']
 
 coreml_mnist = coremltools.converters.keras.convert(
-    './models/modelv4.8_12_channels_last.categories.h5', input_names=['image'], output_names=['output'], 
+    './models/model_v7_0_final.h5', input_names=['image'], output_names=['output'], 
     class_labels=output_labels, image_input_names='image')
-coreml_mnist.save('modelv4v8_12.categories.mlmodel')
+coreml_mnist.save('./models/model_v7_1_final.mlmodel')
 
 
